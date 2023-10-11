7264629b47a7b74ba638fd7e644d33570b69be41
Dmytro Kulinich <spo1091@i.ua>
2023-10-11 10:12:14 +0300

    Add project

---
+++ PickRandomCards/PickRandomCards/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using PickRandomCards;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Скільки карт ти хочеш взяти?");
+        string answer = Console.ReadLine();
+        if (int .TryParse(answer, out int numberOfCards))
+        {
+            string[] result = new string[numberOfCards];
+            result = CardPicker.PickSomeCard(numberOfCards);
+            foreach (string card in result)
+            {
+                Console.WriteLine(card);
+            }
+        }
+        else
+        {
+            Console.WriteLine("ТИ НЕ ВКАЗАВ ЧИСЛО!!!!");
+        }
+    }
+}
